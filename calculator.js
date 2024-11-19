95497a428c334785291f2808429c41c1247ab24e
ATANU MANDAL <atanu2697@gmail.com>
2024-11-20 00:59:33 +0530

    Add files via upload
    It is a import-export basic concept using json.
    json:-
             ***"type": module,***

---
+++ calculator.js
@@ -0,0 +1,7 @@
+import { add , sub } from "./functions.js";
+
+const sum = add(1, 2);
+const substraction = sub(1, 2);
+
+console.log(sum);
+console.log(substraction);
---
+++ functions.js
@@ -0,0 +1,14 @@
+export function add(a, b) {
+    return a + b;
+}
+
+const sum = add(1, 2);
+console.log(sum);
+
+
+export function sub(a, b) {
+    return a - b;
+}
+
+const substraction = sub(1, 2);
+console.log(substraction);
